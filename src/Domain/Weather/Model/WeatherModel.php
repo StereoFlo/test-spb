8be804551eefdef76864d6ee170a9884d94d5f34
StereoFlo <StereoFlo@users.noreply.github.com>
2018-12-27 08:43:23 +0300

    fix

--- src/Domain/Weather/Model/WeatherModel.php
+++ src/Domain/Weather/Model/WeatherModel.php
@@ -40,6 +40,11 @@ class WeatherModel
      */
     public function getWeek(): ?array
     {
-        return $this->weatherRepo->getWeek(Carbon::now()->year, Carbon::now()->month, Carbon::now()->startOfWeek()->day, Carbon::now()->endOfWeek()->day);
+        $year = Carbon::now()->year;
+        $month = Carbon::now()->month;
+        $weekStart = Carbon::now()->startOfWeek(Carbon::MONDAY)->day;
+        $weekEnd   = Carbon::now()->endOfWeek(Carbon::SUNDAY)->day;
+
+        return $this->weatherRepo->getWeek($year, $month, $weekStart, $weekEnd);
     }
 }
